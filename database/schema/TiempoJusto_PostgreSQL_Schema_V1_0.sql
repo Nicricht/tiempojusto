89b2e689b8623521677af69a60b698c41b5c5d3d
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-09 12:40:39 -0300

    db: reemplazar placeholder por loader completo del schema

--- database/schema/TiempoJusto_PostgreSQL_Schema_V1_0.sql
+++ database/schema/TiempoJusto_PostgreSQL_Schema_V1_0.sql
@@ -1,1 +1,16 @@
-PLACEHOLDER
+-- TIEMPOJUSTO PostgreSQL/PostGIS Schema V1.0
+-- Loader modular del schema físico completo.
+-- Ejecutar con psql desde esta carpeta:
+--   psql -d tiempojusto -f TiempoJusto_PostgreSQL_Schema_V1_0.sql
+--
+-- Los fragmentos conservan exactamente el orden del artefacto original de 1530 líneas.
+
+\ir parts/part_00.sql
+\ir parts/part_01.sql
+\ir parts/part_02.sql
+\ir parts/part_03.sql
+\ir parts/part_04.sql
+\ir parts/part_05.sql
+\ir parts/part_06.sql
+\ir parts/part_07.sql
+\ir parts/part_08.sql
