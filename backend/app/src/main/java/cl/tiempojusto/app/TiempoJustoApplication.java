50ca99356b548579bb4a160d81c4d5b9f19dbf80
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-11 13:50:55 -0300

    feat(runtime): agregar bootstrap Spring Boot

---
+++ backend/app/src/main/java/cl/tiempojusto/app/TiempoJustoApplication.java
@@ -0,0 +1,11 @@
+package cl.tiempojusto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiempoJustoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TiempoJustoApplication.class, args);
+    }
+}
